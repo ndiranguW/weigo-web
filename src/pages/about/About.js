df64b4243753619463ba876aa9582987f9705633
ndiranguW <ndiranguwillbeckh@gmail.com>
2023-01-15 17:17:16 +0300

    create website pages

---
+++ src/pages/about/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles.css";
+
+export default function About() {
+  return (
+    <div className="about container">
+      <div className="mx-auto p-4 rounded">
+        <h2 className="text-center">About Us</h2>
+        <p>
+          WeiGo helps businesses get rid of their manual paper work processes.
+          When you go paperless and automate your workflow processes with WeiGo,
+          you get work done faster while significantly cutting costs.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/client.js
@@ -0,0 +1,8 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "25xh2j08",
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "v2021-10-21"
+});
