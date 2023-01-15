13f55a5826dc4a7e8cbdda7c24a9278e59a2149f
ndiranguW <ndiranguwillbeckh@gmail.com>
2023-01-15 17:29:46 +0300

    update application files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+import { Home } from "./pages";
+import { About } from "./pages";
+import { Contact } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="main">
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
