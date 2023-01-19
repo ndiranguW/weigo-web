4894616f30469bf3a5ae9bd9aac2f18bec4c5593
Willbeckh <wileyhndirangu04@gmail.com>
2023-01-19 16:46:41 +0300

    update schema imports.

--- weigo-app/schemas/index.ts
+++ weigo-app/schemas/index.ts
@@ -1,4 +1,7 @@
 import home from './home'
 import about from './about'
+import features from './features'
+import blog from "./blog"
+import author from "./author"
 
-export const schemaTypes = [home, about]
+export const schemaTypes = [home, about, features, blog, author]
