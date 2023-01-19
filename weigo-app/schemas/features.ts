74f14ea888aae82848ca880b2bf412a1a865bc67
Willbeckh <wileyhndirangu04@gmail.com>
2023-01-19 16:44:31 +0300

    create features components.

---
+++ weigo-app/schemas/features.ts
@@ -0,0 +1,22 @@
+export default {
+  name: 'features',
+  title: 'Features',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Feature',
+      type: 'string',
+    },
+    {
+      name: 'about',
+      title: 'Feature Description',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'featureImg',
+      type: 'image',
+    },
+  ],
+}
