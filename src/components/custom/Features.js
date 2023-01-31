5d95ff8d742c78facc379093117ac9c8bb9d831e
Willbeckh <wileyhndirangu04@gmail.com>
2023-01-31 15:43:36 +0300

    create footer component, create+updte features components

---
+++ src/components/custom/Features.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import sanityClient from "../../client";
+
+const Features = () => {
+  const [featureContent, setFeatureContent] = useState([]);
+
+  // query sanity
+
+  useEffect(() => {
+    const query = `*[_type == 'features']{
+          name,
+        about,
+        image{
+          asset->{url}
+        },
+      }`;
+
+    sanityClient
+      .fetch(query)
+      .then((response) => setFeatureContent(response))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="features-container p-4">
+      <div className="container mt-5">
+        <div className="feature-page-text w-50 mx-auto">
+          <h2 className="text-center">
+            The all-in-one platform to manage your construction.
+          </h2>
+          <p className="text-center text-secondary">
+            lorem ipsum dolor amet!lorem ipsum dolor amet! lorem ipsum dolor
+            amet! lorem ipsum dolor amet!
+          </p>
+        </div>
+        <div className="row d-flex justify-content-center mt-4">
+          {featureContent.map((feature, index) => (
+            <div key={index} className="col-sm-12 col-md-8 col-lg-3">
+              <div className="card text-center">
+                <img
+                  src={feature.image.asset.url}
+                  alt="card icon"
+                  className="feature-card__img mx-auto"
+                />
+                <div className="card-body">
+                  <h5 className="card-title"> {feature.name}</h5>
+                  <p className="card-text">{feature.about}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
