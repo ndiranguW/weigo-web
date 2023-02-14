d81ce44aec62ccabc49cd969d1f00d98b38473c4
Willbeckh <wileyhndirangu04@gmail.com>
2023-02-15 00:29:53 +0300

    untrack sanity client file.

--- src/client.js
+++
@@ -1,9 +0,0 @@
-import sanityClient from "@sanity/client";
-
-export default sanityClient({
-  projectId: "25xh2j08",
-  dataset: "production",
-  useCdn: true,
-  apiVersion: "v2021-10-21",
-  apiUrl: "https://weigo.sanity.studio"
-});
