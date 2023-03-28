b293c6bd609ec209d500aa15afba2b4e32bf76e4
Willbeckh <wileyhndirangu04@gmail.com>
2023-03-29 02:18:51 +0300

    update waitlist banner

--- src/components/custom/Contact.js
+++ src/components/custom/Contact.js
@@ -5,21 +5,21 @@ const Contact = () => {
   return (
     <div className="container p-5">
       <div className="row p-2 rounded contact-form">
-        <h3 className="text-center text-light fs-4">Subscribe to our newsletter.</h3>
+        <h3 className="text-center text-light fs-4">Join Waitlist</h3>
         <div className="col-sm-12 col-lg-6 mx-auto ">
           <form>
             <div className="input-group input-group-md m-2">
               <input
                 type={"email"}
                 className="form-control"
-                placeholder="doe@example.com"
+                placeholder="john@company.com"
               />
               <button
                 type="submit"
                 value={"submit"}
-                className="btn btn-primary"
+                className="btn btn-primary w-25"
               >
-                Subscribe
+                Join
               </button>
             </div>
           </form>
