a51fbc689fbcb9cb68ccfdf4374185ff64af5f15
Willbeckh <wileyhndirangu04@gmail.com>
2023-03-29 02:20:25 +0300

    create contact feedback form

--- src/components/custom/Footer.js
+++ src/components/custom/Footer.js
@@ -1,17 +1,21 @@
 import React from "react";
 import logo from "../../assets/logo-white.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTwitter, faLinkedin, faFacebook } from "@fortawesome/free-brands-svg-icons";
+import {
+  faTwitter,
+  faLinkedin,
+  faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
 import { faAt, faLocationPin } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="container-fluid footer">
-      <div className="container  p-2">
+      <div className="container p-2">
         <div className="row">
-          <div className="col-sm-4 col-md-6 col-lg-4 ">
-            <img src={logo} alt="footer logo" className="footer-logo " />
+          <div className="col-sm-12 col-md-8 col-lg-3">
+            <img src={logo} alt="footer logo" className="footer-logo" />
             <div className="footer__contact-details">
               <div className="">
                 <p>
@@ -22,7 +26,7 @@ const Footer = () => {
                   <FontAwesomeIcon icon={faAt} color="#fd7e14" />
                   &nbsp; hello@weigo.io
                 </p>
-                <div className="socials">
+                <div className="socials mt-4">
                   <a
                     href="https://www.facebook.com/profile.php?id=100090567519450&mibextid=LQQJ4d"
                     className="footer-links"
@@ -60,9 +64,53 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="col-sm-6 col-md-6 col-lg-8 mt-5">
+
+          {/* footer contact form */}
+          <div className="col-sm-12 col-md-8 col-lg-4 mt-5 me-4">
+            <h3 style={{ color: "#fd7e14" }}>Feedback.</h3>
+            <form>
+              <div className="form__group">
+                <div className="mb-2">
+                  <input
+                    type={"text"}
+                    placeholder="Your Name"
+                    className="form__field"
+                    required
+                  />
+                </div>
+                <div className="mb-2">
+                  <input
+                    type={"text"}
+                    placeholder="Title & Company name"
+                    className="form__field"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type={"email"}
+                    placeholder="Your Email"
+                    className="form__field"
+                    required
+                  />
+                </div>
+                <div className="mb-2">
+                  <textarea
+                    className="form__field"
+                    placeholder="Your comments or feedback"
+                    rows={"1"}
+                    required
+                  ></textarea>
+                </div>
+                <div className="mb-3">
+                  <button className="demo-btn w-100">Send</button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div className="col-sm-12 col-md-4 col-lg-2 mt-5">
             <h4 style={{ color: "#fd7e14" }}>Quick Links</h4>
-            <div className="navbar-nav p-0">
+            <div className="navbar-nav">
               <NavLink
                 to="/"
                 exact="true"
@@ -78,6 +126,7 @@ const Footer = () => {
               </NavLink>
             </div>
           </div>
+
           <hr />
           <div className="footer-line">
             <div className="text-center">
