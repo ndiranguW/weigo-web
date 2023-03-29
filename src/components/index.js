b4cd57e12ffc9b85083dcc431e4fb1fb799ee86b
Willbeckh <wileyhndirangu04@gmail.com>
2023-03-29 04:27:40 +0300

    add code documentation comments.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Navbar } from "./navbar/Header";
+
+// export all custom components, can be imported as named imports in any external component
+// * eg: import {Contact} from '<path>/component'
 export { default as Contact } from "./custom/Contact";
 export { default as AboutPurpose } from "./custom/AboutPurpose";
