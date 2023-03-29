804b13fb8273c0c1c383551f632f00a0a9450fed
Willbeckh <wileyhndirangu04@gmail.com>
2023-03-29 04:28:28 +0300

    ch: create a maintenence mode component.

---
+++ src/Misc/Maintenance.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Maintenance = () => {
+  return (
+    <div className="container">
+      <p>
+        Oops sorry for the inconvinience. Matter has been taken by the red team.
+      </p>
+      <p>We will be back in short while.</p>
+    </div>
+  );
+};
+
+export default Maintenance;
